fc59ca5385f4736f8dbe4f6bba4d0577551bdfb5
Testando0 <joh769637@gmail.com>
2025-12-25 11:41:35 -0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
                 "Authorization": `Bearer ${apiKey}`
             },
             body: JSON.stringify({
-                model: "deepseek-r1-distill-llama-32b",
+                model: "openai/gpt-oss-120b",
                 messages: [
                     { 
                         role: "system", 
